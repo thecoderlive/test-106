ba233645ee641debf5e3be98f8d411c8a8e8b7f9
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-10-04 04:23:39 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "79796a23-5eec-453b-88f6-328d55319e26",
+    "home_feed": [
+        {
+            "id": "1441c5bd-64f5-4ba5-945a-cd61529bd564",
+            "artist_name": "century",
+            "home_carousel": [
+                {
+                    "id": "1d6dadcf-2c76-491c-9968-f620cf00f8e2",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/309.png"
+                },
+                {
+                    "id": "10b28e98-139f-4844-ae6b-9f80418b7b41",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/34.png"
+                },
+                {
+                    "id": "67452978-b5ea-4fe9-ba0e-f57ff77ea28d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                },
+                {
+                    "id": "b0bdad2e-9b58-47a4-9e30-6b801529b0ee",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/183.png"
+                },
+                {
+                    "id": "ca067f97-be97-4a79-88fe-db7a6f4c465c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/299.png"
+                }
+            ],
+            "art_caption": "memory",
+            "art_description": "Any window at push form sit color."
+        },
+        {
+            "id": "3885c62c-b91f-485d-aaac-9e6c65996fd0",
+            "artist_name": "turn",
+            "home_carousel": [
+                {
+                    "id": "67ffea83-ad95-49bb-89e5-e7d750b622f6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/29.png"
+                },
+                {
+                    "id": "54b3aff7-d7e9-41c0-be3f-074ed18746e3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "42e15e77-4f8a-43a0-819f-f679864fb900",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/191.png"
+                },
+                {
+                    "id": "fa36c0ed-8280-4c1e-a8e6-3823b5643154",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/261.png"
+                },
+                {
+                    "id": "158b4dc3-94b1-49f5-a5cb-2a4734a13b50",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/105.png"
+                }
+            ],
+            "art_caption": "last",
+            "art_description": "Movie tell where six. Actually take my away."
+        },
+        {
+            "id": "51d5c98b-bb44-442f-972b-7be0ea7cbee6",
+            "artist_name": "industry",
+            "home_carousel": [
+                {
+                    "id": "c363aa72-6740-4102-8810-29170091c646",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/190.png"
+                },
+                {
+                    "id": "873c531b-ea5e-4ea1-a0f0-ea8d9fb491bc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/137.png"
+                },
+                {
+                    "id": "d7c8a239-f768-46b7-95d2-3e241fc45a0e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/25.png"
+                },
+                {
+                    "id": "abe2fb7c-3c3e-4941-82d9-9251869e356d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/91.png"
+                },
+                {
+                    "id": "b6f304f3-1cf8-415a-8896-9662d16d94ca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/292.png"
+                }
+            ],
+            "art_caption": "parent",
+            "art_description": "History have explain street cold rather."
+        },
+        {
+            "id": "4eef01e9-64d1-41a8-a71e-472d3c46fc1a",
+            "artist_name": "window",
+            "home_carousel": [
+                {
+                    "id": "1d34c3a5-896b-47d5-9e0b-45544cfa71d8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/228.png"
+                },
+                {
+                    "id": "279265e3-ef44-450e-976a-e7f00fad224f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/57.png"
+                },
+                {
+                    "id": "47c5f172-df65-494a-90ce-c8e18171e73f",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/306.png"
+                },
+                {
+                    "id": "135a277a-05dd-4339-a0c2-f98a72d71976",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/267.png"
+                },
+                {
+                    "id": "361a1c7c-ec70-4bff-a37b-327057a5754b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/308.png"
+                }
+            ],
+            "art_caption": "attorney",
+            "art_description": "Act interview drug. Wrong read able subject case."
+        },
+        {
+            "id": "8a48e847-02e3-4c84-a579-30f51c0993ff",
+            "artist_name": "production",
+            "home_carousel": [
+                {
+                    "id": "055594f9-e55d-41c0-b7bd-d3e791ad72d3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/45.png"
+                },
+                {
+                    "id": "887cd669-f79b-4a94-8661-5f77a81ad40c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/285.png"
+                },
+                {
+                    "id": "a8e488f2-f617-48e6-9d0d-f06c65e2643c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/12.png"
+                },
+                {
+                    "id": "53da75be-4084-470a-9971-772517eea144",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/175.png"
+                },
+                {
+                    "id": "474fb81f-ea26-4814-9a8d-390e4ae412ff",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/78.png"
+                }
+            ],
+            "art_caption": "kind",
+            "art_description": "South bill light practice example."
+        }
+    ]
+}
